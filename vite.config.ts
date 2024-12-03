8ea855ef2dffcf1acdb2dee37f0e1fcdf7c8d41b
Fellyph Cintra <fellyph.cintra@gmail.com>
2024-12-03 18:44:14 +0000

    moving project to typescript

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: resolve(__dirname, 'popup.html'),
-        background: resolve(__dirname, 'background.js'),
+        background: resolve(__dirname, 'background.ts'),
       },
       output: {
         entryFileNames: '[name].js',
